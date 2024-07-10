44d9e3b9d95288d91e6954f5bff1bbad6b376e87
Moisés Xavier <secondmox91@gmail.com>
2024-07-10 03:30:56 -0300

    feat: Adiciona um botao para copiar o texto do chat

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const textarea = document.querySelector(".input-text");
 const btnEncrypt = document.querySelector(".btn-encrypt");
 const btnDecrypt = document.querySelector(".btn-decrypt");
+const textSection = document.querySelector(".chat-texts");
 
 textarea.addEventListener(
   "input",
   () => {
-    this.style.height = "auto";
-    this.style.height = this.scrollHeight + "px";
-    this.style.maxHeight = 250 + "px";
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+    textarea.style.maxHeight = 250 + "px";
   },
   false
 );
@@ -21,7 +22,7 @@ btnEncrypt.addEventListener("click", (e) => {
   const response = encryptText(textMinus);
   chat[chat.length] = ["Texto Criptografado", response];
   textarea.value = "";
-  console.log(chat);
+  addTextToChat();
 });
 
 btnDecrypt.addEventListener("click", (e) => {
@@ -31,19 +32,17 @@ btnDecrypt.addEventListener("click", (e) => {
   const response = decryptText(textMinus);
   chat[chat.length] = ["Texto Descriptografado", response];
   textarea.value = "";
-  console.log(chat);
-  console.log(response);
+  addTextToChat();
 });
 
-const encryptedWords = {
-  e: "enter",
-  i: "imes",
-  a: "ai",
-  o: "ober",
-  u: "ufat",
-};
-
 function encryptText(text) {
+  const encryptedWords = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
   let textEncrypted = "";
   for (let i = 0; i < text.length; i++) {
     const letters = text[i];
@@ -60,3 +59,43 @@ function decryptText(text) {
   text = text.replace(/ufat/g, "u");
   return text;
 }
+
+function addTextToChat() {
+  textSection.innerHTML = "";
+  for (let [index, [title, content]] of chat.entries()) {
+    console.log(title);
+    console.log(content);
+    createText(title, content);
+  }
+}
+
+function createText(title, content) {
+  const text = document.createElement("div");
+  text.classList.add("text");
+
+  const textTitle = document.createElement("h5");
+  textTitle.classList.add("text-title");
+  textTitle.innerText = title;
+  text.appendChild(textTitle);
+
+  const textContent = document.createElement("p");
+  textContent.classList.add("text-content");
+  textContent.innerText = content;
+  text.appendChild(textContent);
+
+  const btnCopy = document.createElement("div");
+  btnCopy.classList.add("btn-copy");
+
+  const spanIconCopy = document.createElement("span");
+  spanIconCopy.classList.add("material-symbols-outlined");
+  spanIconCopy.innerText = "content_copy";
+  spanIconCopy.addEventListener("click", () => {
+    navigator.clipboard.writeText(textContent.innerText);
+  });
+  btnCopy.appendChild(spanIconCopy);
+  text.appendChild(btnCopy);
+
+  textSection.appendChild(text);
+}
+
+// addTextToChat();
